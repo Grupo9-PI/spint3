0402886935ed4423ee05714b59f025d97770f16d
Erickdavi097 <erick.silva097@sptech.school>
2025-06-03 15:28:34 -0300

    Mudei coisas decorativas só

--- site Lumaris/app.js
+++ site Lumaris/app.js
@@ -17,10 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,24 +26,26 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
-    console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
+    console.log("\x1b[36m\x1b[3m%s\x1b[0m",`
+_______________________________________________________________________________________________________________
+
+            ###      ###   ###     #       #        ###       #########      #####    ##########  
+            # #      # #   # #    # #     # #     # # # #     # ###### #     ## ##   #  ########        
+            # #      # #   # #    # # # # # #    # #   # #    # #     # #     # #   #  # 
+            # #      # #   # #    # #  #  # #   # #     # #   # ###### #      # #    # ########   
+            # #      # #   # #    # #     # #   # ####### #   # #### #        # #     #######  #  
+            # #      #  ###  #    # #     # #   # #     # #   # #    # #      # #           #  # 
+            # #####   #     #     # #     # #   # #     # #   # #     # #    ## ##   #######  # 
+            #######    #####      ###     ###   ###     ###   ###      ###   #####   #########  
+
+_______________________________________________________________________________________________________________`);
+    console.log(`\n          
+    A API sendo utilizada é a WEB-DATA-VIZ!                                                                                 
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
-    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
+    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`)
 });
